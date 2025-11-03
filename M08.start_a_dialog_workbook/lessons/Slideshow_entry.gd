e3842d5fd94a34084ddd319336784055a10d287e
ProbablyJaylan <jaxie1@s.sfusd.edu>
2025-11-03 14:52:59 -0800

    M8L5 halfway point
    halfway to 70% done

---
+++ M08.start_a_dialog_workbook/lessons/Slideshow_entry.gd
@@ -0,0 +1,5 @@
+class_name SlideShowEntry extends Resource
+
+@export var expression := preload("res://assets/emotion_sad.png")
+@export var character := preload("res://assets/sophia.png")
+@export var text := ""
