10fc2b39122b6c44318d7cbcdeca9c1e6edcf7f0
ProbablyJaylan <jaxie1@s.sfusd.edu>
2025-11-05 11:24:46 -0800

    Module8L5 done
    Im free

--- M08.start_a_dialog_workbook/lessons/Slideshow_entry.gd
+++ M08.start_a_dialog_workbook/lessons/Slideshow_entry.gd
@@ -2,4 +2,4 @@ class_name SlideShowEntry extends Resource
 
 @export var expression := preload("res://assets/emotion_sad.png")
 @export var character := preload("res://assets/sophia.png")
-@export var text := ""
+@export_multiline var text := ""
